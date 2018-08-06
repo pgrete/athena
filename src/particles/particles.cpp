pgrete/athena

commit 52cfda971d3c91c27047c4c9c2e1a591f8076951

Restructure receive buffers in Particles class.
Now irecv and rrecv are pointers instead of AthenaArrays.  Also, they
first iterate over properties then over particles, so that they can be
used as buffers in MPI receives.

src/particles/particles.cpp
@@ -125,8 +125,8 @@ Particles::Particles(MeshBlock *pmb, ParameterInput *pin)
   // Allocate buffers.
   nprecvmax = 1;
   nprecv = 0;
-  irecv.NewAthenaArray(nint,nprecvmax);
-  rrecv.NewAthenaArray(nreal+naux,nprecvmax);
+  irecv = new long [nprecvmax * nint];
+  rrecv = new Real [nprecvmax * (nreal + naux)];
 }
 
 //--------------------------------------------------------------------------------------
@@ -151,8 +151,8 @@ Particles::~Particles()
   delete ppm;
 
   // Delete buffers.
-  irecv.DeleteAthenaArray();
-  rrecv.DeleteAthenaArray();
+  delete [] irecv;
+  delete [] rrecv;
 }
 
 //--------------------------------------------------------------------------------------
@@ -478,19 +478,18 @@ void Particles::SendToNeighbors()
     }
 
     // Check the buffer size of the target MeshBlock.
-    if (pnp->nprecv >= pnp->nprecvmax) {
-      pnp->nprecvmax *= 2;
-      pnp->irecv.ResizeLastDimension(pnp->nprecvmax);
-      pnp->rrecv.ResizeLastDimension(pnp->nprecvmax);
-    }
+    if (pnp->nprecv >= pnp->nprecvmax)
+      pnp->FlushReceiveBuffer();
 
     // Copy the properties of the particle to the neighbor.
+    long *pi = pnp->irecv + nint * pnp->nprecv;
     for (int j = 0; j < nint; ++j)
-      pnp->irecv(j,pnp->nprecv) = intprop(j,k);
+      *pi++ = intprop(j,k);
+    Real *pr = pnp->rrecv + (nreal + naux) * pnp->nprecv;
     for (int j = 0; j < nreal; ++j)
-      pnp->rrecv(j,pnp->nprecv) = realprop(j,k);
-    for (int i = nreal, j = 0; j < naux; ++i, ++j)
-      pnp->rrecv(i,pnp->nprecv) = auxprop(j,k);
+      *pr++ = realprop(j,k);
+    for (int j = 0; j < naux; ++j)
+      *pr++ = auxprop(j,k);
     ++pnp->nprecv;
 
     // Pop the particle from the current MeshBlock.
@@ -530,15 +529,16 @@ void Particles::FlushReceiveBuffer()
   }
 
   // Flush the receive buffers.
-  for (int j = 0; j < nint; ++j)
-    for (long ip = npar, k = 0; k < nprecv; ++ip, ++k)
-      intprop(j,ip) = irecv(j,k);
-  for (int j = 0; j < nreal; ++j)
-    for (long ip = npar, k = 0; k < nprecv; ++ip, ++k)
-      realprop(j,ip) = rrecv(j,k);
-  for (int i = nreal, j = 0; j < naux; ++i, ++j)
-    for (long ip = npar, k = 0; k < nprecv; ++ip, ++k)
-      auxprop(j,ip) = rrecv(i,k);
+  long *pi = irecv;
+  Real *pr = rrecv;
+  for (long k = npar; k < npar + nprecv; ++k) {
+    for (int j = 0; j < nint; ++j)
+      intprop(j,k) = *pi++;
+    for (int j = 0; j < nreal; ++j)
+      realprop(j,k) = *pr++;
+    for (int j = 0; j < naux; ++j)
+      auxprop(j,k) = *pr++;
+  }
 
   // Find their position indices.
   AthenaArray<Real> xps, yps, zps, xi1s, xi2s, xi3s;

src/particles/particles.hpp
@@ -126,10 +126,10 @@ private:
   bool active1_, active2_, active3_;  // active dimensions
 
   // MeshBlock-to-MeshBlock communication:
-  AthenaArray<long> irecv;  // integer receive buffers
-  AthenaArray<Real> rrecv;  // real receive buffers
-  int nprecvmax;            // maximum number of particles per receive buffer
-  int nprecv;               // actual number of particles per receive buffer
+  long *irecv;    // integer receive buffer
+  Real *rrecv;    // real receive buffer
+  int nprecvmax;  // maximum number of particles per receive buffer
+  int nprecv;     // actual number of particles per receive buffer
 };
 
 //--------------------------------------------------------------------------------------

commit 7b7d6dc19f77997fbb47d3763208c72fd1a1674a

Use ParticleBuffer in Particles.
This commit also fixed a bug introduced in commit 52cfda97, where the
receive buffer should not be flushed in the middle of sending particles.

src/particles/particles.cpp
@@ -124,11 +124,8 @@ Particles::Particles(MeshBlock *pmb, ParameterInput *pin)
   // Shallow copy to shorthands.
   AssignShorthands();
 
-  // Allocate buffers.
-  nprecvmax = 1;
-  nprecv = 0;
-  irecv = new long [nprecvmax * nint];
-  rrecv = new Real [nprecvmax * (nreal + naux)];
+  // Initiate ParticleBuffer class.
+  ParticleBuffer::SetNumberOfProperties(nint, nreal + naux);
 }
 
 //--------------------------------------------------------------------------------------
@@ -151,10 +148,6 @@ Particles::~Particles()
 
   // Delete mesh auxiliaries.
   delete ppm;
-
-  // Delete buffers.
-  delete [] irecv;
-  delete [] rrecv;
 }
 
 //--------------------------------------------------------------------------------------
@@ -320,7 +313,7 @@ void Particles::SendParticlesAndMesh(int step)
 void Particles::ReceiveParticlesAndMesh(int step)
 {
   // Flush Particles receive buffer.
-  if (nprecv > 0) FlushReceiveBuffer();
+  if (recv.npar > 0) FlushReceiveBuffer();
 
   // Flush ParticleMesh receive buffers and deposit MeshAux to MeshBlock.
   if (ppm->nmeshaux > 0) {
@@ -480,21 +473,20 @@ void Particles::SendToNeighbors()
     }
 
     // Check the buffer size of the target MeshBlock.
-    if (pnp->nprecv >= pnp->nprecvmax) {
-      pnp->FlushReceiveBuffer();
-      pnp->DoubleReceiveBuffer();
-    }
+    ParticleBuffer& nrecv = pnp->recv;
+    if (nrecv.npar >= nrecv.nparmax)
+      nrecv.Reallocate((nrecv.nparmax > 0) ? 2 * nrecv.nparmax : 1);
 
     // Copy the properties of the particle to the neighbor.
-    long *pi = pnp->irecv + nint * pnp->nprecv;
+    long *pi = nrecv.ibuf + ParticleBuffer::nint * nrecv.npar;
     for (int j = 0; j < nint; ++j)
       *pi++ = intprop(j,k);
-    Real *pr = pnp->rrecv + (nreal + naux) * pnp->nprecv;
+    Real *pr = nrecv.rbuf + ParticleBuffer::nreal * nrecv.npar;
     for (int j = 0; j < nreal; ++j)
       *pr++ = realprop(j,k);
     for (int j = 0; j < naux; ++j)
       *pr++ = auxprop(j,k);
-    ++pnp->nprecv;
+    ++nrecv.npar;
 
     // Pop the particle from the current MeshBlock.
     if (--npar != k) {
@@ -518,6 +510,7 @@ void Particles::SendToNeighbors()
 void Particles::FlushReceiveBuffer()
 {
   // Check the memory size.
+  int nprecv = recv.npar;
   if (npar + nprecv > nparmax) {
     // Increase maximum number of particles allowed.
     nparmax += 2 * (npar + nprecv - nparmax);
@@ -533,8 +526,8 @@ void Particles::FlushReceiveBuffer()
   }
 
   // Flush the receive buffers.
-  long *pi = irecv;
-  Real *pr = rrecv;
+  long *pi = recv.ibuf;
+  Real *pr = recv.rbuf;
   for (long k = npar; k < npar + nprecv; ++k) {
     for (int j = 0; j < nint; ++j)
       intprop(j,k) = *pi++;
@@ -556,21 +549,7 @@ void Particles::FlushReceiveBuffer()
 
   // Clear the receive buffers.
   npar += nprecv;
-  nprecv = 0;
-}
-
-//--------------------------------------------------------------------------------------
-//! \fn void Particles::DoubleReceiveBuffer()
-//  \brief doubles the receive buffer; any content in the buffer will be lost.
-
-void Particles::DoubleReceiveBuffer()
-{
-  delete [] irecv;
-  delete [] rrecv;
-  nprecvmax *= 2;
-  irecv = new long [nprecvmax * nint];
-  rrecv = new Real [nprecvmax * (nreal + naux)];
-  nprecv = 0;
+  recv.npar = 0;
 }
 
 //--------------------------------------------------------------------------------------

src/particles/particles.hpp
@@ -13,6 +13,7 @@
 #include "../athena_arrays.hpp"
 #include "../mesh/mesh.hpp"
 #include "../outputs/outputs.hpp"
+#include "particle_buffer.hpp"
 #include "particle-mesh.hpp"
 
 class ParameterInput;
@@ -117,7 +118,6 @@ private:
   void ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3);
   void EulerStep(Real t, Real dt, const AthenaArray<Real>& meshsrc);
   void FlushReceiveBuffer();
-  void DoubleReceiveBuffer();
   void SaveStatus();
   void SendToNeighbors();
 
@@ -125,10 +125,7 @@ private:
   bool active1_, active2_, active3_;  // active dimensions
 
   // MeshBlock-to-MeshBlock communication:
-  long *irecv;    // integer receive buffer
-  Real *rrecv;    // real receive buffer
-  int nprecvmax;  // maximum number of particles per receive buffer
-  int nprecv;     // actual number of particles per receive buffer
+  ParticleBuffer recv;  // receive buffer
 };
 
 //--------------------------------------------------------------------------------------