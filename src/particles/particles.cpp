pgrete/athena

commit 9147388e8101bd64bb7baae35cbc18489b94c42c

Make all long's to int's for number of particles.

src/particles/particles.cpp
@@ -274,7 +274,7 @@ void Particles::SendToNeighbors()
     return;
   }
 
-  for (long k = 0; k < npar; ) {
+  for (int k = 0; k < npar; ) {
     // Check if a particle is outside the boundary.
     int xi1i = int(xi1(k)), xi2i = int(xi2(k)), xi3i = int(xi3(k));
     int ox1 = active1_ ? CheckSide(xi1i, IS, IE) : 0,
@@ -324,7 +324,7 @@ void Particles::SendToNeighbors()
       ppb->Reallocate((ppb->nparmax > 0) ? 2 * ppb->nparmax : 1);
 
     // Copy the properties of the particle to the buffer.
-    long *pi = ppb->ibuf + ParticleBuffer::nint * ppb->npar;
+    int *pi = ppb->ibuf + ParticleBuffer::nint * ppb->npar;
     for (int j = 0; j < nint; ++j)
       *pi++ = intprop(j,k);
     Real *pr = ppb->rbuf + ParticleBuffer::nreal * ppb->npar;
@@ -509,11 +509,11 @@ bool Particles::ReceiveParticleMesh(int stage)
 }
 
 //--------------------------------------------------------------------------------------
-//! \fn void Particles::ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3)
+//! \fn void Particles::ApplyBoundaryConditions(int k, Real &x1, Real &x2, Real &x3)
 //  \brief applies boundary conditions to particle k and returns its updated mesh
 //         coordinates (x1,x2,x3).
 
-void Particles::ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3)
+void Particles::ApplyBoundaryConditions(int k, Real &x1, Real &x2, Real &x3)
 {
   bool flag = false;
   RegionSize& mesh_size = pmy_mesh->mesh_size;
@@ -620,7 +620,7 @@ void Particles::ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3)
 }
 
 //--------------------------------------------------------------------------------------
-//! \fn void Particles::GetPositionIndices(MeshBlock *pmb, long npar,
+//! \fn void Particles::GetPositionIndices(MeshBlock *pmb, int npar,
 //                                         const AthenaArray<Real>& xp,
 //                                         const AthenaArray<Real>& yp,
 //                                         const AthenaArray<Real>& zp,
@@ -629,7 +629,7 @@ void Particles::ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3)
 //                                         AthenaArray<Real>& xi3)
 //  \brief finds the position indices of each particle with respect to the local grid.
 
-void Particles::GetPositionIndices(MeshBlock *pmb, long npar,
+void Particles::GetPositionIndices(MeshBlock *pmb, int npar,
                                    const AthenaArray<Real>& xp,
                                    const AthenaArray<Real>& yp,
                                    const AthenaArray<Real>& zp,
@@ -637,7 +637,7 @@ void Particles::GetPositionIndices(MeshBlock *pmb, long npar,
                                    AthenaArray<Real>& xi2,
                                    AthenaArray<Real>& xi3)
 {
-  for (long k = 0; k < npar; ++k) {
+  for (int k = 0; k < npar; ++k) {
     // Convert to the Mesh coordinates.
     Real x1, x2, x3;
     _CartesianToMeshCoords(xp(k), yp(k), zp(k), x1, x2, x3);
@@ -658,7 +658,7 @@ void Particles::EulerStep(Real t, Real dt, const AthenaArray<Real>& meshsrc)
   AddAcceleration(t, dt, meshsrc);
 
   // Update the positions and velocities **from the beginning of the time step**.
-  for (long k = 0; k < npar; ++k) {
+  for (int k = 0; k < npar; ++k) {
     xp(k) = xp0(k) + dt * vpx(k);
     yp(k) = yp0(k) + dt * vpy(k);
     zp(k) = zp0(k) + dt * vpz(k);
@@ -674,7 +674,7 @@ void Particles::EulerStep(Real t, Real dt, const AthenaArray<Real>& meshsrc)
 
 void Particles::SaveStatus()
 {
-  for (long k = 0; k < npar; ++k) {
+  for (int k = 0; k < npar; ++k) {
     // Save current positions.
     xp0(k) = xp(k);
     yp0(k) = yp(k);
@@ -693,7 +693,7 @@ void Particles::SaveStatus()
 
 void Particles::ZeroAcceleration()
 {
-  for (long k = 0; k < npar; ++k) {
+  for (int k = 0; k < npar; ++k) {
     apx(k) = 0.0;
     apy(k) = 0.0;
     apz(k) = 0.0;
@@ -752,9 +752,9 @@ void Particles::FlushReceiveBuffer(ParticleBuffer& recv)
   }
 
   // Flush the receive buffers.
-  long *pi = recv.ibuf;
+  int *pi = recv.ibuf;
   Real *pr = recv.rbuf;
-  for (long k = npar; k < npar + nprecv; ++k) {
+  for (int k = npar; k < npar + nprecv; ++k) {
     for (int j = 0; j < nint; ++j)
       intprop(j,k) = *pi++;
     for (int j = 0; j < nreal; ++j)
@@ -854,7 +854,7 @@ void Particles::NewBlockTimeStep()
 
   // Find the allowed time step for each particle.
   Real dt_inv2_max = 0.0, dt_inv2;
-  for (long k = 0; k < npar; ++k) {
+  for (int k = 0; k < npar; ++k) {
     dt_inv2 = 0.0;
     if (active1_) dt_inv2 += std::pow(vpx(k) / pc->dx1f(int(xi1(k))), 2);
     if (active2_) dt_inv2 += std::pow(vpy(k) / pc->dx2f(int(xi2(k))), 2);
@@ -876,7 +876,7 @@ void Particles::NewBlockTimeStep()
 size_t Particles::GetSizeInBytes()
 {
   size_t size = sizeof(npar);
-  if (npar > 0) size += npar * (nint * sizeof(long) + nreal * sizeof(Real));
+  if (npar > 0) size += npar * (nint * sizeof(int) + nreal * sizeof(Real));
   return size;
 }
 
@@ -901,7 +901,7 @@ void Particles::ReadRestart(char *mbdata, int &os)
 
   if (npar > 0) {
     // Read integer properties.
-    size_t size = npar * sizeof(long);
+    size_t size = npar * sizeof(int);
     for (int k = 0; k < nint; ++k) {
       std::memcpy(&(intprop(k,0)), &(mbdata[os]), size);
       os += size;
@@ -928,7 +928,7 @@ void Particles::WriteRestart(char *&pdata)
 
   if (npar > 0) {
     // Write integer properties.
-    size_t size = npar * sizeof(long);
+    size_t size = npar * sizeof(int);
     for (int k = 0; k < nint; ++k) {
       std::memcpy(pdata, &(intprop(k,0)), size);
       pdata += size;
@@ -981,7 +981,7 @@ void Particles::FormattedTableOutput(Mesh *pm, OutputParameters op)
     os << "# Athena++ particle data at time = " << pm->time << std::endl;
 
     // Write the particle data in the meshblock.
-    for (long k = 0; k < ppar->npar; ++k)
+    for (int k = 0; k < ppar->npar; ++k)
       os << ppar->pid(k) << "  "
          << ppar->xp(k) << "  " << ppar->yp(k) << "  " << ppar->zp(k) << "  "
          << ppar->vpx(k) << "  " << ppar->vpy(k) << "  " << ppar->vpz(k) << std::endl;

src/particles/particle-mesh.cpp
@@ -169,12 +169,12 @@ void ParticleMesh::InterpolateMeshToParticles(
   Real *pp[nprop];
   for (int n = 0; n < nprop; ++n) {
     Real *p = pp[n] = &par(p1+n,0);
-    for (long k = 0; k < ppar_->npar; ++k)
+    for (int k = 0; k < ppar_->npar; ++k)
       *p++ = 0.0;
   }
 
   // Loop over each particle.
-  for (long k = 0; k < ppar_->npar; ++k) {
+  for (int k = 0; k < ppar_->npar; ++k) {
     // Find the domain the particle influences.
     Real xi1 = ppar_->xi1(k), xi2 = ppar_->xi2(k), xi3 = ppar_->xi3(k);
     int ix1s = int(xi1 - dxi1_), ix1e = int(xi1 + dxi1_);
@@ -225,7 +225,7 @@ void ParticleMesh::AssignParticlesToMeshAux(
   }
 
   // Loop over each particle.
-  for (long k = 0; k < ppar_->npar; ++k) {
+  for (int k = 0; k < ppar_->npar; ++k) {
     // Find the domain the particle influences.
     Real xi1 = ppar_->xi1(k) - (active1_ ? OFFSET : 0),
          xi2 = ppar_->xi2(k) - (active2_ ? OFFSET : 0),
@@ -295,7 +295,7 @@ void ParticleMesh::InterpolateMeshAndAssignParticles(
   Real *pp[ni];
   for (int n = 0; n < ni; ++n) {
     Real *p = pp[n] = &pardst(pd1+n,0);
-    for (long k = 0; k < ppar_->npar; ++k)
+    for (int k = 0; k < ppar_->npar; ++k)
       *p++ = 0.0;
   }
 
@@ -313,7 +313,7 @@ void ParticleMesh::InterpolateMeshAndAssignParticles(
   }
 
   // Loop over each particle.
-  for (long k = 0; k < ppar_->npar; ++k) {
+  for (int k = 0; k < ppar_->npar; ++k) {
     // Find the domain the particle influences.
     Real xi1 = ppar_->xi1(k), xi2 = ppar_->xi2(k), xi3 = ppar_->xi3(k);
     int imb1s = int(xi1 - dxi1_), imb1e = int(xi1 + dxi1_);
@@ -701,7 +701,7 @@ void ParticleMesh::AssignParticlesToDifferentLevels(
     }
 
     // Find particles that influences the neighbor block.
-    for (long k = 0; k < ppar_->npar; ++k) {
+    for (int k = 0; k < ppar_->npar; ++k) {
       Real xi1 = ppar_->xi1(k), xi2 = ppar_->xi2(k), xi3 = ppar_->xi3(k);
       if ((active1_ && (xi1 <= ba.xi1min || xi1 >= ba.xi1max)) ||
           (active2_ && (xi2 <= ba.xi2min || xi2 >= ba.xi2max)) ||
@@ -731,7 +731,7 @@ void ParticleMesh::AssignParticlesToDifferentLevels(
 
       // Stack the properties of the particle and set the pointers to the buffer.
       Real prop[nprop];
-      long dbuf1 = ix1s + ba.ngx1 * (ix2s + ba.ngx2 * ix3s),
+      int dbuf1 = ix1s + ba.ngx1 * (ix2s + ba.ngx2 * ix3s),
            dbuf2 = ba.ngx1 - ix1e + ix1s - 1,
            dbuf3 = ba.ngx1 * (ba.ngx2 - ix2e + ix2s - 1);
 

src/particles/particle_buffer.cpp
@@ -74,7 +74,7 @@ ParticleBuffer::ParticleBuffer(int nparmax0)
 
   // Initialize the instance variables.
   nparmax = nparmax0;
-  ibuf = new long [nint * nparmax];
+  ibuf = new int [nint * nparmax];
   rbuf = new Real [nreal * nparmax];
   npar = 0;
 #ifdef MPI_PARALLEL
@@ -131,12 +131,12 @@ void ParticleBuffer::Reallocate(int new_nparmax)
 
   // Allocate new space.
   nparmax = new_nparmax;
-  long *ibuf_new = new long [nint * nparmax];
+  int *ibuf_new = new int [nint * nparmax];
   Real *rbuf_new = new Real [nreal * nparmax];
 
   // Move existing data.
   if (npar > 0) {
-    std::memcpy(ibuf_new, ibuf, nint * npar * sizeof(long));
+    std::memcpy(ibuf_new, ibuf, nint * npar * sizeof(int));
     std::memcpy(rbuf_new, rbuf, nreal * npar * sizeof(Real));
   }
 

src/particles/particle_buffer.hpp
@@ -40,7 +40,7 @@ protected:
   static int nreal;  // number of real properties per particle
 
   // Instance variables
-  long* ibuf;   // ptr to integer buffer
+  int* ibuf;   // ptr to integer buffer
   Real* rbuf;   // ptr to real buffer
   int nparmax;  // maximum number of particles
   int npar;     // actual number of particles in the buffer

src/particles/particles.hpp
@@ -105,11 +105,11 @@ protected:
                              AthenaArray<Real>& meshdst) {}
 
   // Instance variables
-  long npar;     // number of particles
-  long nparmax;  // maximum number of particles per meshblock
+  int npar;     // number of particles
+  int nparmax;  // maximum number of particles per meshblock
 
                                // Data attached to the particles:
-  AthenaArray<long> intprop;   //   integer properties
+  AthenaArray<int> intprop;   //   integer properties
   AthenaArray<Real> realprop;  //   real properties
   AthenaArray<Real> auxprop;   //   auxiliary properties (communicated when
                                //     particles moving to another meshblock)
@@ -118,7 +118,7 @@ protected:
   ParticleMesh *ppm;  // ptr to particle-mesh
 
                                        // Shorthands:
-  AthenaArray<long> pid;               //   particle ID
+  AthenaArray<int> pid;               //   particle ID
   AthenaArray<Real> xp, yp, zp;        //   position
   AthenaArray<Real> vpx, vpy, vpz;     //   velocity
   AthenaArray<Real> xi1, xi2, xi3;     //   position indices in local meshblock
@@ -131,7 +131,7 @@ protected:
 
 private:
   // Class methods
-  static void GetPositionIndices(MeshBlock *pmb, long npar,
+  static void GetPositionIndices(MeshBlock *pmb, int npar,
                                  const AthenaArray<Real>& xp,
                                  const AthenaArray<Real>& yp,
                                  const AthenaArray<Real>& zp,
@@ -140,7 +140,7 @@ private:
                                  AthenaArray<Real>& xi3);
 
   // Instance methods
-  void ApplyBoundaryConditions(long k, Real &x1, Real &x2, Real &x3);
+  void ApplyBoundaryConditions(int k, Real &x1, Real &x2, Real &x3);
   void EulerStep(Real t, Real dt, const AthenaArray<Real>& meshsrc);
   void FlushReceiveBuffer(ParticleBuffer& recv);
   void SaveStatus();

src/pgen/uniform_streaming.cpp
@@ -53,11 +53,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
 
   // Find the total number of particles in each direction.
   RegionSize& mesh_size = pmy_mesh->mesh_size;
-  long npx1 = (block_size.nx1 > 1) ?
+  int npx1 = (block_size.nx1 > 1) ?
                   pin->GetOrAddInteger("problem", "npx1", mesh_size.nx1) : 1,
-       npx2 = (block_size.nx2 > 1) ?
+      npx2 = (block_size.nx2 > 1) ?
                   pin->GetOrAddInteger("problem", "npx2", mesh_size.nx2) : 1,
-       npx3 = (block_size.nx3 > 1) ?
+      npx3 = (block_size.nx3 > 1) ?
                   pin->GetOrAddInteger("problem", "npx3", mesh_size.nx3) : 1;
 
   // Find the mass of each particle and the distance between adjacent particles.
@@ -79,8 +79,8 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
   ppar->mass = dtog * vol / (npx1 * npx2 * npx3);
 
   // Find the local number of particles and their beginning index.
-  long ix1 = 0, ix2 = 0, ix3 = 0;
-  long npx1_loc = 1, npx2_loc = 1, npx3_loc = 1;
+  int ix1 = 0, ix2 = 0, ix3 = 0;
+  int npx1_loc = 1, npx2_loc = 1, npx3_loc = 1;
   if (block_size.nx1 > 1) {
     ix1 = lround((block_size.x1min - mesh_size.x1min) / dx1);
     npx1_loc = lround((block_size.x1max - block_size.x1min) / dx1);
@@ -104,7 +104,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
   }
 
   // Assign the particles.
-  long ipar = 0, id = ix1 + npx1 * (ix2 + npx2 * ix3);
+  int ipar = 0, id = ix1 + npx1 * (ix2 + npx2 * ix3);
   for (int k = 0; k < npx3_loc; ++k) {
     Real zp1 = block_size.x3min + (k + 0.5) * dx3;
     for (int j = 0; j < npx2_loc; ++j) {

commit 1d982f93bc4d603796b09eb30b1a5382116b5e16

Fix a bug introduced in commit 9147388e8.
The MPI send/recv of integer properties also needed to be changed.

src/particles/particles.cpp
@@ -358,7 +358,7 @@ void Particles::SendToNeighbors() {
       MPI_Send(&npsend, 1, MPI_INT, nb.rank, send.tag, my_comm);
       if (npsend > 0) {
         MPI_Request req = MPI_REQUEST_NULL;
-        MPI_Isend(send.ibuf, npsend * ParticleBuffer::nint, MPI_LONG,
+        MPI_Isend(send.ibuf, npsend * ParticleBuffer::nint, MPI_INT,
                   dst, send.tag + 1, my_comm, &req);
         MPI_Request_free(&req);
         MPI_Isend(send.rbuf, npsend * ParticleBuffer::nreal, MPI_ATHENA_REAL,
@@ -427,7 +427,7 @@ bool Particles::ReceiveFromNeighbors() {
         // Receive data from the neighbor.
         if (!recv.flagi) {
           if (recv.reqi == MPI_REQUEST_NULL)
-            MPI_Irecv(recv.ibuf, recv.npar * ParticleBuffer::nint, MPI_LONG,
+            MPI_Irecv(recv.ibuf, recv.npar * ParticleBuffer::nint, MPI_INT,
                       nb.rank, recv.tag + 1, my_comm, &recv.reqi);
           else
             MPI_Test(&recv.reqi, &recv.flagi, MPI_STATUS_IGNORE);